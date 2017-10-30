48f035a5e88cac3ae4b744166e61507d19d82626
owent <owt5008137@live.com>
2017-10-30 12:26:36 +0800

    1. 修订ifndef-guard，不再使用保留字 2. 移除过老编译器的支持，统一加#pragma once

--- ini_loader.h
+++ ini_loader.h
@@ -22,62 +22,60 @@
  *   2016-04-14: Section部分也支持使用.来分割层级
  */
 
-#ifndef _UTIL_CONFIG_INI_INILOADER_H_
-#define _UTIL_CONFIG_INI_INILOADER_H_
+#ifndef UTIL_CONFIG_INI_INILOADER_H
+#define UTIL_CONFIG_INI_INILOADER_H
 
-#if defined(_MSC_VER) && (_MSC_VER >= 1020)
-# pragma once
-#endif
+#pragma once
 
+#include <list>
+#include <map>
+#include <sstream>
 #include <stdint.h>
 #include <string>
-#include <sstream>
-#include <map>
-#include <list>
 #include <vector>
+
 
 // ================= 版本号 =================
 #define LIBINILOADER_VERSION "1.0.1.0"
 
 namespace util {
-    namespace config {        
+    namespace config {
         // ================= 错误码 =================
-        enum EN_INILOADER_ERROR_CODE
-        {
+        enum EN_INILOADER_ERROR_CODE {
             EIEC_SUCCESS = 0,
             EIEC_OPENFILE = -1,
         };
         // ----------------- 错误码 -----------------
 
         // ================= 存储层 =================
-        class ini_value
-        {
+        class ini_value {
         public:
             typedef std::map<std::string, ini_value> node_type;
+
         private:
             std::vector<std::string> _data;
             node_type _chirldren_nodes;
 
 
-            template<typename _Tt>
-            inline void clear_data(_Tt& data) const {}
+            template <typename _Tt>
+            inline void clear_data(_Tt &data) const {}
 
-            inline void clear_data(char*& data) const { data = NULL; }
-            inline void clear_data(std::string& data) const { data.clear(); }
-            inline void clear_data(bool& data) const { data = false; }
-            inline void clear_data(char& data) const { data = 0; }
-            inline void clear_data(short& data) const { data = 0; }
-            inline void clear_data(int& data) const { data = 0; }
-            inline void clear_data(long& data) const { data = 0; }
-            inline void clear_data(long long& data) const { data = 0; }
-            inline void clear_data(unsigned char& data) const { data = 0; }
-            inline void clear_data(unsigned short& data) const { data = 0; }
-            inline void clear_data(unsigned int& data) const { data = 0; }
-            inline void clear_data(unsigned long& data) const { data = 0; }
-            inline void clear_data(unsigned long long& data) const { data = 0; }
+            inline void clear_data(char *&data) const { data = NULL; }
+            inline void clear_data(std::string &data) const { data.clear(); }
+            inline void clear_data(bool &data) const { data = false; }
+            inline void clear_data(char &data) const { data = 0; }
+            inline void clear_data(short &data) const { data = 0; }
+            inline void clear_data(int &data) const { data = 0; }
+            inline void clear_data(long &data) const { data = 0; }
+            inline void clear_data(long long &data) const { data = 0; }
+            inline void clear_data(unsigned char &data) const { data = 0; }
+            inline void clear_data(unsigned short &data) const { data = 0; }
+            inline void clear_data(unsigned int &data) const { data = 0; }
+            inline void clear_data(unsigned long &data) const { data = 0; }
+            inline void clear_data(unsigned long long &data) const { data = 0; }
 
-            template<typename _Tt>
-            inline _Tt string2any(const std::string& data) const {
+            template <typename _Tt>
+            inline _Tt string2any(const std::string &data) const {
                 _Tt ret;
                 clear_data(ret);
                 if (!data.empty()) {
@@ -91,22 +89,22 @@ namespace util {
         public:
             ini_value();
 
-            void add(const std::string& val);
-            void add(const char* begin, const char* end);
+            void add(const std::string &val);
+            void add(const char *begin, const char *end);
 
             // 节点操作
-            bool empty() const; // like stl
+            bool empty() const;    // like stl
             bool has_data() const; // like stl
-            size_t size() const; // like stl
-            void clear(); // like stl
-            ini_value& operator[](const std::string key);
-            node_type& get_children();
-            const node_type& get_children() const;
+            size_t size() const;   // like stl
+            void clear();          // like stl
+            ini_value &operator[](const std::string key);
+            node_type &get_children();
+            const node_type &get_children() const;
 
-            static const std::string& get_empty_string();
+            static const std::string &get_empty_string();
 
             // 数值转换操作
-            template<typename _Tt>
+            template <typename _Tt>
             inline _Tt as(size_t index = 0) const {
                 if (index < _data.size()) {
                     return string2any<_Tt>(_data[index]);
@@ -116,7 +114,7 @@ namespace util {
             }
 
             // 获取存储对象的字符串
-            const std::string& as_cpp_string(size_t index = 0) const;
+            const std::string &as_cpp_string(size_t index = 0) const;
 
             char as_char(size_t index = 0) const;
 
@@ -132,7 +130,7 @@ namespace util {
 
             float as_float(size_t index = 0) const;
 
-            const char* as_string(size_t index = 0) const;
+            const char *as_string(size_t index = 0) const;
 
             unsigned char as_uchar(size_t index = 0) const;
 
@@ -165,98 +163,88 @@ namespace util {
         // ================= 词法状态机 =================
         namespace analysis {
             // space
-            struct spaces
-            {
+            struct spaces {
                 static bool test_char(char c);
-                bool test(const char* begin, const char* end);
-                const char* parse(const char* begin, const char* end);
+                bool test(const char *begin, const char *end);
+                const char *parse(const char *begin, const char *end);
             };
 
             // comment
-            struct comment
-            {
-                bool test(const char* begin, const char* end);
-                const char* parse(const char* begin, const char* end);
+            struct comment {
+                bool test(const char *begin, const char *end);
+                const char *parse(const char *begin, const char *end);
             };
 
             // identify
-            struct identify
-            {
-                const char* _begin_ptr;
-                const char* _end_ptr;
+            struct identify {
+                const char *_begin_ptr;
+                const char *_end_ptr;
 
-                bool test(const char* begin, const char* end);
-                const char* parse(const char* begin, const char* end);
+                bool test(const char *begin, const char *end);
+                const char *parse(const char *begin, const char *end);
             };
 
             // key
-            struct key
-            {
-                typedef std::list<std::pair<const char*, const char*> > list_type;
+            struct key {
+                typedef std::list<std::pair<const char *, const char *> > list_type;
                 list_type _keys;
 
-                bool test(const char* begin, const char* end);
-                const char* parse(const char* begin, const char* end);
+                bool test(const char *begin, const char *end);
+                const char *parse(const char *begin, const char *end);
             };
 
             // section
-            struct section
-            {
-                typedef std::list<std::pair<const char*, const char*> > list_type;
+            struct section {
+                typedef std::list<std::pair<const char *, const char *> > list_type;
                 list_type _keys;
 
-                bool test(const char* begin, const char* end);
-                const char* parse(const char* begin, const char* end);
+                bool test(const char *begin, const char *end);
+                const char *parse(const char *begin, const char *end);
             };
 
             // string
-            struct string
-            {
+            struct string {
                 static char _convert_map[1 << (sizeof(char) * 8)];
                 void init_conver_map();
 
                 std::string _value;
 
-                bool test(const char* begin, const char* end);
-                const char* parse(const char* begin, const char* end, bool enable_convert = false);
+                bool test(const char *begin, const char *end);
+                const char *parse(const char *begin, const char *end, bool enable_convert = false);
             };
 
             // value
-            struct value
-            {
+            struct value {
                 std::string _value;
 
-                bool test(const char* begin, const char* end);
-                const char* parse(const char* begin, const char* end);
+                bool test(const char *begin, const char *end);
+                const char *parse(const char *begin, const char *end);
             };
 
             // expression
-            struct expression
-            {
+            struct expression {
                 key _key;
                 value _value;
 
-                bool test(const char* begin, const char* end);
-                const char* parse(const char* begin, const char* end);
+                bool test(const char *begin, const char *end);
+                const char *parse(const char *begin, const char *end);
             };
 
             // sentence
-            struct sentence
-            {
+            struct sentence {
                 std::pair<bool, section> _sect;
                 std::pair<bool, expression> _exp;
 
-                bool test(const char* begin, const char* end);
-                const char* parse(const char* begin, const char* end);
+                bool test(const char *begin, const char *end);
+                const char *parse(const char *begin, const char *end);
             };
-        }
+        } // namespace analysis
         // ----------------- 词法状态机 -----------------
 
-        class ini_loader
-        {
+        class ini_loader {
         private:
             ini_value _root_node; // root node
-            ini_value* _current_node_ptr;
+            ini_value *_current_node_ptr;
 
         public:
             ini_loader();
@@ -269,7 +257,7 @@ namespace util {
              * @return 返回码
              * @see EN_INILOADER_ERROR_CODE
              */
-            int load_stream(std::istream& in, bool is_append = false);
+            int load_stream(std::istream &in, bool is_append = false);
 
             /**
              * @brief 从文件取数据
@@ -278,7 +266,7 @@ namespace util {
              * @return 返回码
              * @see EN_INILOADER_ERROR_CODE
              */
-            int load_file(const char* file_path, bool is_append = false);
+            int load_file(const char *file_path, bool is_append = false);
 
             /**
              * @brief 从文件取数据
@@ -287,7 +275,7 @@ namespace util {
              * @return 返回码
              * @see EN_INILOADER_ERROR_CODE
              */
-            int load_file(const std::string& file_path, bool is_append = false);
+            int load_file(const std::string &file_path, bool is_append = false);
 
             /**
              * @brief 清空
@@ -298,31 +286,31 @@ namespace util {
              * @brief 设置当前配置结构根节点路径
              * @param path 路径
              */
-            void set_section(const std::string& path);
+            void set_section(const std::string &path);
 
             /**
              * @brief 获取当前配置结构根节点
              * @return 当前配置结构根节点
              */
-            ini_value& get_section();
+            ini_value &get_section();
 
             /**
              * @brief 获取当前配置结构根节点
              * @return 当前配置结构根节点
              */
-            const ini_value& get_section() const;
+            const ini_value &get_section() const;
 
             /**
              * @brief 获取根节点
              * @return 根节点
              */
-            ini_value& get_root_node();
+            ini_value &get_root_node();
 
             /**
              * @brief 获取根节点
              * @return 根节点
              */
-            const ini_value& get_root_node() const;
+            const ini_value &get_root_node() const;
 
             /**
              * @brief 根据目录获取子节点
@@ -331,7 +319,7 @@ namespace util {
              * @return 子节点
              * @note 如果子节点不存在会创建空列表节点
              */
-            ini_value& get_node(const std::string& path, ini_value* father_ptr = NULL);
+            ini_value &get_node(const std::string &path, ini_value *father_ptr = NULL);
 
             /**
              * @brief 根据子节点名称获取子节点
@@ -340,7 +328,7 @@ namespace util {
              * @return 子节点
              * @note 如果子节点不存在会创建空列表节点
              */
-            ini_value& get_child_node(const std::string& path, ini_value* father_ptr = NULL);
+            ini_value &get_child_node(const std::string &path, ini_value *father_ptr = NULL);
 
             // ========================= 单值容器转储 =========================
 
@@ -351,9 +339,9 @@ namespace util {
              * @param is_force 是否是强制转储，强制在找不到路径对应的配置项时会尝试清空数据
              * @param index 转储索引，默认是第一个值
              */
-            template<typename Ty>
-            void dump_to(const std::string& path, Ty& val, bool is_force = false, size_t index = 0) {
-                ini_value& cur_node = get_node(path);
+            template <typename Ty>
+            void dump_to(const std::string &path, Ty &val, bool is_force = false, size_t index = 0) {
+                ini_value &cur_node = get_node(path);
                 if (cur_node.has_data() || is_force) {
                     val = cur_node.as<Ty>(index);
                 }
@@ -366,7 +354,7 @@ namespace util {
              * @param is_force 是否是强制转储，强制在找不到路径对应的配置项时会尝试清空数据
              * @param index 转储索引，默认是第一个值
              */
-            void dump_to(const std::string& path, bool& val, bool is_force = false, size_t index = 0);
+            void dump_to(const std::string &path, bool &val, bool is_force = false, size_t index = 0);
 
             /**
              * @brief 配置转储
@@ -375,17 +363,17 @@ namespace util {
              * @param is_force 是否是强制转储，强制在找不到路径对应的配置项时会尝试清空数据
              * @param index 转储索引，默认是第一个值
              */
-            void dump_to(const std::string& path, std::string& val, bool is_force = false, size_t index = 0);
+            void dump_to(const std::string &path, std::string &val, bool is_force = false, size_t index = 0);
 
             /**
-            * @brief 配置转储 - 字符串
-            * @param path 配置路径
-            * @param begin 转储目标起始地址
-            * @param end 转储目标边界地址
-            * @param is_force 是否是强制转储，强制在找不到路径对应的配置项时会尝试清空数据
-            * @param index 转储索引，默认是第一个值
-            */
-            void dump_to(const std::string& path, char* begin, char* end, bool is_force = false, size_t index = 0);
+             * @brief 配置转储 - 字符串
+             * @param path 配置路径
+             * @param begin 转储目标起始地址
+             * @param end 转储目标边界地址
+             * @param is_force 是否是强制转储，强制在找不到路径对应的配置项时会尝试清空数据
+             * @param index 转储索引，默认是第一个值
+             */
+            void dump_to(const std::string &path, char *begin, char *end, bool is_force = false, size_t index = 0);
 
             /**
              * @brief 配置转储 - 字符串
@@ -394,8 +382,8 @@ namespace util {
              * @param is_force 是否是强制转储，强制在找不到路径对应的配置项时会尝试清空数据
              * @param index 转储索引，默认是第一个值
              */
-            template<size_t MAX_COUNT>
-            void dump_to(const std::string& path, char(&val)[MAX_COUNT], bool is_force = false, size_t index = 0) {
+            template <size_t MAX_COUNT>
+            void dump_to(const std::string &path, char (&val)[MAX_COUNT], bool is_force = false, size_t index = 0) {
                 dump_to(path, val, val + MAX_COUNT, is_force, index);
             }
 
@@ -407,16 +395,16 @@ namespace util {
              * @param is_force 是否是强制转储，强制在找不到路径对应的配置项时会尝试清空数据
              * @note 容器足够大时，会尝试转储所有配置
              */
-            template<typename Ty>
-            void dump_to(const std::string& path, std::vector<Ty>& val, bool is_force = false) {
+            template <typename Ty>
+            void dump_to(const std::string &path, std::vector<Ty> &val, bool is_force = false) {
                 if (is_force) {
                     val.clear();
                 }
 
-                ini_value& cur_node = get_node(path);
+                ini_value &cur_node = get_node(path);
                 for (size_t i = 0; i < cur_node.size(); ++i) {
                     val.push_back(Ty());
-                    Ty& new_node = val.back();
+                    Ty &new_node = val.back();
                     dump_to(path, new_node, is_force, i);
                 }
             }
@@ -428,16 +416,16 @@ namespace util {
              * @param is_force 是否是强制转储，强制在找不到路径对应的配置项时会尝试清空数据
              * @note 容器足够大时，会尝试转储所有配置
              */
-            template<typename Ty>
-            void dump_to(const std::string& path, std::list<Ty>& val, bool is_force = false) {
+            template <typename Ty>
+            void dump_to(const std::string &path, std::list<Ty> &val, bool is_force = false) {
                 if (is_force) {
                     val.clear();
                 }
 
-                ini_value& cur_node = get_node(path);
+                ini_value &cur_node = get_node(path);
                 for (size_t i = 0; i < cur_node.size(); ++i) {
                     val.push_back(Ty());
-                    Ty& new_node = val.back();
+                    Ty &new_node = val.back();
                     dump_to(path, new_node, is_force, i);
                 }
             }
@@ -449,9 +437,9 @@ namespace util {
              * @param is_force 是否是强制转储，强制在找不到路径对应的配置项时会尝试清空数据
              * @note 容器足够大时，会尝试转储所有配置
              */
-            template<typename Ty, size_t MAX_COUNT>
-            void dump_to(const std::string& path, Ty (&val)[MAX_COUNT], bool is_force = false) {
-                ini_value& cur_node = get_node(path);
+            template <typename Ty, size_t MAX_COUNT>
+            void dump_to(const std::string &path, Ty (&val)[MAX_COUNT], bool is_force = false) {
+                ini_value &cur_node = get_node(path);
                 for (size_t i = 0; i < cur_node.size() && i < MAX_COUNT; ++i) {
                     dump_to(path, val[i], is_force, i);
                 }
@@ -465,16 +453,16 @@ namespace util {
              * @param is_force 是否是强制转储，强制在找不到路径对应的配置项时会尝试清空数据
              * @note 容器足够大时，会尝试转储所有配置
              */
-            template<typename TIter>
-            void dump_to(const std::string& path, TIter begin, TIter end, bool is_force = false) {
+            template <typename TIter>
+            void dump_to(const std::string &path, TIter begin, TIter end, bool is_force = false) {
                 size_t index = 0;
-                ini_value& cur_node = get_node(path);
+                ini_value &cur_node = get_node(path);
                 for (TIter i = begin; i != end && index < cur_node.size(); ++index, ++i) {
                     dump_to(path, *i, is_force, index);
                 }
             }
         };
-    }
-}
+    } // namespace config
+} // namespace util
 
 #endif
